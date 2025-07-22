d284d6678b01652def51b7066a8aadca6d5c4311
Iva Horn <iva.horn@icloud.com>
2025-07-22 20:49:12 +0200

    Initial Commit

---
+++ Framecloud/FramecloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  FramecloudApp.swift
+//  Framecloud
+//
+//  Created by Iva Horn on 22.07.25.
+//
+
+import SwiftUI
+
+@main
+struct FramecloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
